3451f2c326e953887cc14a29f252a587193cb42a
Kumar Gaurav <kumargaurav8521899767@gmail.com>
2026-05-23 09:17:07 +0530

    add /api

--- car-rental-platform/frontend/src/services/api.js
+++ car-rental-platform/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://carrentalplatform.duckdns.org';
+const API_BASE_URL = 'https://carrentalplatform.duckdns.org/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
