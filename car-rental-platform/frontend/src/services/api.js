1fe222ff06c9e6755e998f06d065cc3a583c2953
Kumar Gaurav <kumargaurav8521899767@gmail.com>
2026-05-23 08:37:37 +0530

    add aws deployed url

--- car-rental-platform/frontend/src/services/api.js
+++ car-rental-platform/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://car-rental-e9te.onrender.com/api';
+const API_BASE_URL = 'http://13.51.195.112:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
